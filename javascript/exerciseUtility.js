0b2b317c1317d3d9e5fcd73cf9070f3b691334f1
GianlucaMurro <gianluca.murro0@gmail.com>
2024-10-03 19:18:48 +0200

    added exercise 3

--- javascript/exerciseUtility.js
+++ javascript/exerciseUtility.js
@@ -2,9 +2,9 @@
 function control(a) {
   while (a != parseInt(a)) {
     if (a == parseFloat(a)) {
-      a = prompt("The value given was not an integer, retry");
+      a = prompt(`The value given was not an integer, retry`);
     } else {
-      a = prompt("The value given was not a number, retry");
+      a = prompt(`The value given was not a number, retry`);
     }
   }
   return a;
@@ -18,8 +18,44 @@ function display(text, times) {
 //Exercise 2
 function isNumerical(a) {
   while (a != parseInt(a) || a != parseFloat(a)) {
-    if (String(a).toUpperCase() == "EXIT") break;
-    a = prompt("The value given was not a number, retry");
+    if (String(a).toUpperCase() == `EXIT`) break;
+    a = prompt(`The value given was not a number, retry`);
   }
   return a;
+}
+
+//Exercise 3
+function addProductTo(shoppingList) {
+  let product = prompt(`Insert a product you want to add to your shopping list`).toLowerCase();
+  if (shoppingList.includes(product)) {
+    console.log(shoppingList);
+    alert(`${product} is already in your shopping list`);
+  } else {
+    shoppingList.push(product);
+    console.log(shoppingList);
+    alert(`${product} was successfully added to your shopping list`);
+  }
+}
+function removeProductFrom(shoppingList) {
+  let product = prompt(`Insert a product you want to remove from your shopping list`).toLowerCase();
+  if (shoppingList.includes(product)) {
+    i = shoppingList.indexOf(product);
+    shoppingList.splice(i, 1);
+    console.log(shoppingList);
+    alert(`${product} was successfully removed from your shopping list`);
+  } else {
+    console.log(shoppingList);
+    alert(`${product} is not in your shopping list`);
+  }
+}
+function check() {
+  switch (String(prompt(`Do you want to keep going?`)).toUpperCase()) {
+    case "YES":
+      return true;
+    case "NO":
+      return false;
+    default:
+      alert(`I'll take that as a no`);
+      return false;
+  }
 }
