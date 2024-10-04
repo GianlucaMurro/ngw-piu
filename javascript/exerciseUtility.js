46a43a23976f9123bbcdb8cbf65f08de258b2955
GianlucaMurro <gianluca.murro0@gmail.com>
2024-10-04 21:12:03 +0200

    added exercise5.js's optional part + modified approach

--- javascript/exerciseUtility.js
+++ javascript/exerciseUtility.js
@@ -84,3 +84,20 @@ function removeElementFrom(shoppingDict) {
     alert(`${element} is not in your shopping list`);
   }
 }
+
+// Exercise 5
+function display(data, dataList) {
+  value = document.getElementById(data).value;
+  if (value == "" || value.replaceAll(" ", "") == "") {
+    dataList.innerHTML = "";
+    document.getElementById("welcome").hidden = true;
+    alert("At least one of the fields was left empty, fill all of them to submit");
+    return false;
+  } else {
+    let info = document.createElement(`li`);
+    info.innerHTML = `${data}: ${value}`;
+    dataList.appendChild(info);
+    document.getElementById("welcome").hidden = false;
+    return true;
+  }
+}
